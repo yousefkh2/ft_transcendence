81c324fedf99035ed36e38faf08772150cc9f2f3
gzovkic <g.zovkic@web.de>
2026-06-06 19:52:46 +0200

    FEAT(lobby): added test printing for player role

---
+++ game/lobby.gd
@@ -0,0 +1,36 @@
+extends Control
+
+@onready var role_label = $role
+
+func _ready():
+	role_label.text = "Waiting for role..."
+
+	if OS.has_feature("web"):
+		JavaScriptBridge.eval("""
+            window.addEventListener('message', (event) => {
+                if (event.data.type === 'player_info') {
+                    window.godotPlayerInfo = event.data;
+                }
+            });
+		""")
+
+func _process(delta):
+
+	if !OS.has_feature("web"):
+		return
+
+	var json = JavaScriptBridge.eval(
+        "JSON.stringify(window.godotPlayerInfo || null)"
+	)
+
+	if json == "null":
+		return
+
+	var data = JSON.parse_string(json)
+
+	if data:
+		role_label.text = "Role: " + str(data.role)
+
+		JavaScriptBridge.eval(
+            "window.godotPlayerInfo = null;"
+		)
