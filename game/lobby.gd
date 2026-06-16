d8a9cba214ce49b88ab85eca20693e3f70903341
gzovkic <g.zovkic@web.de>
2026-06-16 16:26:04 +0200

    REFAC(livingroom): changed from prost iframe to api request

--- game/lobby.gd
+++ game/lobby.gd
@@ -1,33 +1,37 @@
 extends Control
 
 @onready var role_label = $role
+@onready var http_request = $HTTPRequest
 
 func _ready():
-	role_label.text = "Waiting for role..."
+	role_label.text = "Loading role..."
 
-	if OS.has_feature("web"):
-		JavaScriptBridge.eval("""
-            window.addEventListener('message', (event) => {
-                if (event.data.type === 'player_info') {
-                    window.godotPlayerInfo = event.data;
-                }
-            });
-		""")
+	http_request.request_completed.connect(_on_request_completed)
 
-func _process(delta):
-
-	if not OS.has_feature("web"):
-		return
-
-	var json = JavaScriptBridge.eval(
-    "JSON.stringify(window.godotPlayerInfo || null)"
+	var err = http_request.request(
+		"http://localhost:8080/api/player"
 	)
 
-	var data = JSON.parse_string(json)
+	if err != OK:
+		role_label.text = "Request failed"
+
+
+func _on_request_completed(
+	result,
+	response_code,
+	headers,
+	body
+):
+
+	if response_code != 200:
+		role_label.text = "Server Error"
+		return
+
+	var text = body.get_string_from_utf8()
+
+	print(text)
+
+	var data = JSON.parse_string(text)
 
 	if data:
 		role_label.text = "Role: " + str(data.role)
-
-		JavaScriptBridge.eval(
-            "window.godotPlayerInfo = null;"
-		)
