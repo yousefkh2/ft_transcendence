e0faecee7b354845210d8d4f8a6a0f13d530bab4
gzovkic <g.zovkic@web.de>
2026-06-29 12:52:10 +0200

    FEAT(livingroom): objects are snaping to 10 diffrent spots

--- game/games/livingroom/scripts/object.gd
+++ game/games/livingroom/scripts/object.gd
@@ -1,23 +1,50 @@
 extends Sprite2D
 
-
 var dragging = false
-var of = Vector2(0,0)
+var drag_offset = Vector2.ZERO
 
+@export var slots_parent: NodePath
 
-var snap = 64
+var slots = []
+var current_slot = null
 
-# Called every frame. 'deltax' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _ready():
+	for child in get_node(slots_parent).get_children():
+		slots.append(child)
+
+func _process(delta):
 	if dragging:
-		#var newPos = get_global_mouse_position() - of
-		position = get_global_mouse_position() - of
-		print(position)
+		global_position = get_global_mouse_position() - drag_offset
 
-
-func _on_button_button_down() -> void:
+func _on_button_button_down():
 	dragging = true
-	of = get_global_mouse_position() - global_position
 
-func _on_button_button_up() -> void:
+	# Free the current slot while dragging
+	if current_slot:
+		current_slot.occupied = false
+		current_slot = null
+
+	drag_offset = get_global_mouse_position() - global_position
+
+func _on_button_button_up():
 	dragging = false
+	snap_to_closest_slot()
+
+func snap_to_closest_slot():
+	var closest = null
+	var closest_distance = INF
+
+	for slot in slots:
+		if slot.occupied:
+			continue
+
+		var dist = global_position.distance_squared_to(slot.global_position)
+
+		if dist < closest_distance:
+			closest_distance = dist
+			closest = slot
+
+	if closest:
+		global_position = closest.global_position
+		closest.occupied = true
+		current_slot = closest
