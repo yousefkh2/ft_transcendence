3b81b36c517eb1a064359f62cff3e3246fdac2d1
gzovkic <g.zovkic@web.de>
2026-08-21 13:24:31 +0200

    FEAT(pregame_lobby): connected lobby with pregame lobby

--- game/games/lobby/scripts/lobby.gd
+++ game/games/lobby/scripts/lobby.gd
@@ -30,3 +30,7 @@ func _on_lobby_button_pressed() -> void:
 
 func _on_select_dev_game_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://games/dev/dev_game_select.tscn")
+
+
+func _on_create_pressed() -> void:
+	get_tree().change_scene_to_file("res://games/pregame_lobby/pregame_lobby.tscn")
