e866ea70fea6d8e3c04090649ef72d53042e35a8
gzovkic <g.zovkic@web.de>
2026-09-16 15:24:37 +0200

    FEAT(web_socket_client.gd): added get_message function to recvie messages from websockets

--- game/games/lobby/web_socket_client.gd
+++ game/games/lobby/web_socket_client.gd
@@ -1,11 +1,53 @@
 extends Node
 
+var socket = WebSocketPeer.new()
+var last_state = WebSocketPeer.STATE_CLOSED
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+signal connected_to_server()
+signal connection_closed()
+signal message_received(message: Variant)
 
+func poll() -> void:
+	if (socket.get_ready_state() != socket.STATE_CLOSED):
+		socket.poll()
+		
+	var state = socket.get_ready_state()
+	
+	if (last_state != state):
+		last_state = state
+		
+		if (state == socket.STATE_OPEN):
+			connected_to_server.emit()
+		elif (state == socket.STATE_CLOSED):
+				connection_closed.emit()
+	
+	while (socket.get_ready_state() == socket.STATE_OPEN and socket.get_available_packet_count()):
+		message_received.emit(get_message())
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func get_message() -> Variant:
+	if (socket.get_available_packet_count() < 1):
+		return null
+		
+	var packet = socket.get_packet()
+	if (socket.was_string_packet()):
+		return (packet.get_string_from_utf8())
+		
+	return (bytes_to_var(packet))
+	
+func send(message) -> int:
 	pass
+	
+func connect_to_url(url) -> int:
+	pass
+
+func close(code := 1000, reason := "") -> void:
+	pass
+
+func clear() -> void:
+	print("clear")
+	
+func get_socket() -> WebSocketPeer:
+	pass
+
+func _process(delta: float) -> void:
+	poll()
